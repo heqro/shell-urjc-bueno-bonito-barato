e313bc0b7592d0e4e75812f8c184abb740c8025f
Heqro <heqromancer@gmail.com>
2020-11-27 21:38:27 +0100

    prueba

---
+++ Proyecto/cmake-build-debug/CMakeFiles/mothershell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mothershell.dir/main.c.o"
+  "mothershell"
+  "mothershell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mothershell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
