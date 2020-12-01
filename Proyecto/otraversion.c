e02264e24359d62a295836635b32b646a3bf6e9d
heqro <58432067+heqro@users.noreply.github.com>
2020-12-01 13:34:31 +0100

    Update otraversion.c

--- Proyecto/otraversion.c
+++ Proyecto/otraversion.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include "parser.h"
@@ -7,13 +8,17 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 void crearPrimero(int pipe[2], tline* line){
-    close(pipe[0]);//nunca utilizaremos el pipe de entrada
-    close(STDIN_FILENO);
+    close(pipe[1]);
+    close(STDOUT_FILENO);
+    dup(pipe[1]);
+    
+    // comprobación de error
     if(line->redirect_input){//hay redirección de entrada
-        pipe[0] = open(line->redirect_input, O_RDONLY);
-    }else{//Escuchar por stdin
-        dup(pipe[0]);
+        int aux = open(line->redirect_input, O_RDONLY);
+        dup2(aux,STDIN_FILENO);
+        close(aux);
     }
+    
 }
 
 void crearUltimo(int pipe[2], tline* line){
@@ -46,6 +51,8 @@ int main() {
     int pipe_ph[2];
     int pipe_hp[2];
 
+    int stdoutAux = dup(1);
+    
 
     printf("msh> ");
     while(fgets(buf, 1024, stdin)){
@@ -63,65 +70,85 @@ int main() {
         
         pipe(pipe_ph); // Creamos pipe para conectar procesos
         pipe(pipe_hp);
+        
         pid = fork();
         if (esHijo(pid)){//Hijo
-			close(pipe_ph[0]);
-			close(pipe_hp[1]);
-			//pipe_ph[1] sera el stdin del hijo
-			close(STDIN_FILENO); //cerramos stdin
-			dup(pipe_ph[1]);
-			close(STDOUT_FILENO);
-			dup(pipe_hp[0]);
-			
-			
-			
-			
-		} else{//Padre
-			
-			close(pipe_ph[1]);
 			close(pipe_hp[0]);
-			//pipe_hp[1] sera el stdin del padre
-			close(STDIN_FILENO); //cerramos stdin
-			if(line->redirect_input){//hay redirección de entrada
-				//funcion comprobar fichero
-				pipe_ph[0] = open(line->redirect_input, O_RDONLY);
-			}else{//Escuchar por stdin
-				dup(pipe_ph[0]);
-			}
-			close(STDOUT_FILENO);
-			dup(pipe_hp[0]);
-			
+            close(pipe_ph[1]);
+            
+            
+            
+            // Caso General
+//             close(STDIN_FILENO);
+//             dup(pipe_ph[0]);
+//             close(STDOUT_FILENO);
+//             dup(pipe_hp[1]);
+		} else{//Padre
+            close(pipe_hp[1]);
+            close(pipe_ph[0]);
+            
+            // Caso general
+//             close(STDIN_FILENO);
+//             dup(pipe_hp[0]);
+//             close(STDOUT_FILENO);
+//             dup(pipe_ph[1]);
 		}
 		
 		
 		
 		
         errorFork(pid);
-        for (i = 1; i <= line->ncommands; i++) {
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+        for (i = 1; i <= line->ncommands; i++) {       
+            if (esHijo(pid)){//Hijo
+                if(i == 2){
+                    // Hacer que el stdin del hijo pase a ser ph[0]
+                    close(STDIN_FILENO);
+                    dup(pipe_ph[0]);
+                    // Hacer que el stdout del hijo lo escribimos en hp[1]
+                    close(pipe_hp[1]);
+                    close(STDOUT_FILENO);
+                    dup(pipe_hp[1]);
+                }
+                if (i == line->ncommands){ // Restaurar STDOUT para poder escribir en él
+                    // Restaurar STDOUT
+                    dup2(stdoutAux, 1);
+                    close(stdoutAux);
+                    // Cerrar pipes, hemos acabado
+                    
+                }
+                if(i % 2 == 0){
+                    // ejecutarComando(i, line);
+                }
+                    
+            } else {//Padre
+                if (i == 1){//leer stdin - redirect_input
+                    crearPrimero(pipe_ph, line);
+                }
+                if (i == 2){
+                    close(pipe_hp[0]);
+                    close(STDIN_FILENO);
+                    dup(pipe_hp[0]);
+                    close(STDOUT_FILENO);
+                    dup(pipe_ph[1]);
+                }
+                if (i == line->ncommands){ // Restaurar STDOUT para poder escribir en él
+                    dup2(stdoutAux, 1);
+                    close(stdoutAux);
+                }
+                
+                if(i % 2 == 1){
+                    //Ejecutar comando
+                    //ejecutarComando(i, line);
+                    for (j=0; j<line->commands[i-1].argc; j++) {
+                        printf("  argumento %d: %s\n", j, line->commands[i-1].argv[j]);
+                    }
+                }
+            }
+            // Si es hijo, cerrar sus descriptores
+            // HIJO: debe cerrar ph[0], hp[1]
+            // Si es padre, cerrar sus descriptores
+            // PADRE: debe cerrar hp[0], ph[1]
+            // Suicidio colectivo
         }
         printf("msh> ");
     }
